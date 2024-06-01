de29ee3839b0005f83266e88e9d6d68548e80de7
eswaribala <Parameswaribala@gmail.com>
2024-06-01 15:45:36 +0530

    Update mysqlconn.pl

--- mysqlconn.pl
+++ mysqlconn.pl
@@ -1,35 +1,35 @@
 use DBI;
- 2 use DBD::Pg
- 3
- 4 # HTTP HEADER
- 5 print "Content-type: text/html \n\n";
- 6
- 7 # CONFIG VARIABLES
- 8 $platform = "postgresql";
- 9 $database = "testdb";
-10 $host = "127.0.0.1";
-11 $port = "5433";
-12 $tablename = "customer";
-13 $user = "postgres";
-14 $pw = "postgres";
-15 my $dsn = "DBI:Pg:dbname = $database;host = $host;port =$port";
-16 #DATA SOURCE NAME
-17 #my $dsn = "dbi:Pg:$database:$host:$port";
-18
-19 # PERL DBI CONNECT
-20 #$DBIconnect = DBI->connect($dsn, $user, $pw);
-21 #$DBIConnect=DBI->connect("dbi:Pg:dbname=$database;host=$host;port=$port", $user, $password);
-22 my $DBIConnect = DBI->connect($dsn, $user, $pw, { RaiseError => 1 })
-23    or die $DBI::errstr;
-24
-25 print "Opened database successfully\n";
-26
-27 print "DB Connected...";
-28
-29 $sth = $DBIConnect->prepare("select * from customer");
-30 $sth->execute();
-31 while ( @row = $sth->fetchrow_array )
-32 {
-33         print "@row\n";
-34 }
-35 sleep(10);
+use DBD::Pg
+
+# HTTP HEADER
+print "Content-type: text/html \n\n";
+
+ # CONFIG VARIABLES
+ $platform = "postgresql";
+ $database = "testdb";
+ $host = "127.0.0.1";
+ $port = "5433";
+ $tablename = "customer";
+ $user = "postgres";
+ $pw = "postgres";
+ my $dsn = "DBI:Pg:dbname = $database;host = $host;port =$port";
+ #DATA SOURCE NAME
+ #my $dsn = "dbi:Pg:$database:$host:$port";
+
+ # PERL DBI CONNECT
+ #$DBIconnect = DBI->connect($dsn, $user, $pw);
+ #$DBIConnect=DBI->connect("dbi:Pg:dbname=$database;host=$host;port=$port", $user, $password);
+ my $DBIConnect = DBI->connect($dsn, $user, $pw, { RaiseError => 1 })
+    or die $DBI::errstr;
+
+ print "Opened database successfully\n";
+
+ print "DB Connected...";
+
+ $sth = $DBIConnect->prepare("select * from customer");
+ $sth->execute();
+ while ( @row = $sth->fetchrow_array )
+ {
+         print "@row\n";
+ }
+ sleep(10);
