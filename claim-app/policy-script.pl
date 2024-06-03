8f255b5e09794215a6e7bfc24b684998c609997d
root <root@ip-172-31-49-60.ec2.internal>
2024-06-03 11:35:57 +0000

    updated

--- claim-app/policy-script.pl
+++ claim-app/policy-script.pl
@@ -1,11 +1,40 @@
 #!/usr/bin/perl -Tw
 use strict;
 use CGI ':standard';
+use DBI;
+use DBD::mysql;
+
+
 
 my $firstname = param('firstName');
 my $lastname = param('lastName');
 my $contact_no = param('contactNo');
 my $adharcard_no=param('adharCardNo');
+
+# CONFIG VARIABLES
+my $platform = "mysql";
+my $database = "edupatientdb";
+my $host = "172.17.0.2";
+my $port = "3306";
+my $tablename = "policy";
+my $user = "root";
+my $pw = "password";
+
+
+
+
+#DATA SOURCE NAME
+my $dsn = "dbi:mysql:$database:$host:$port";
+my $DBIConnect = DBI->connect("DBI:mysql:$database:$host", "$user", "$pw");
+my @records = ( [$firstname, $lastname,$contact_no,$adharcard_no] ) ;
+my $query="insert into policy(first_name,last_name,contact_no,adharcard_no) values(?,?,?,?)";
+my $sth = $DBIConnect->prepare($query);
+#$sth->execute(map { @$_ } @$records);
+foreach my $rec ( @records ) { 
+    $sth->execute( @$rec );
+}
+
+sleep(10);
 
 #=begin
 print header,
